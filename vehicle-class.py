d56c11ab90913c1a6ed1d18a4048612217748c48
lagos-automator1 <somoriboy@gmail.com>
2024-01-21 13:25:13 +0000

    adds functionality to class

--- vehicle-class.py
+++ vehicle-class.py
@@ -6,6 +6,10 @@ class Vehicle():
     def __init__(self, bodystyle):
         self.bodystyle = bodystyle
 
+    def drive(self, speed):
+        self.mode = "driving"
+        self.speed = speed
+
 ## 2 sub-classes - Car & Motorcycle
 # the Car class derives/descends off the Vehicle class
 class Car(Vehicle):
@@ -14,6 +18,10 @@ class Car(Vehicle):
         self.wheels = 4
         self.doors  = 4
         self.enginetype = enginetype
+
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "car at", self.speed)
 
 # the Motorcycle class also derives/descends off the Vehicle class
 class Motorcycle(Vehicle):
@@ -25,6 +33,10 @@ class Motorcycle(Vehicle):
             self.wheels = 2
         self.doors = 0
         self.enginetype = enginetype
+
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "motorcycle at", self.speed)
 
 # Creating instances/objects from the Classes
 car1 = Car("diesel")
@@ -41,3 +53,12 @@ print("Car1 has a " + car1.enginetype + " engine type")
 print(car2.enginetype)
 print(car3.enginetype)
 print(car4.enginetype)
+
+## We call those functions on the objects
+car1.drive(20)
+car2.drive(30)
+car3.drive(45)
+car4.drive(66)
+
+mc1.drive(60)
+mc2.drive(32)
