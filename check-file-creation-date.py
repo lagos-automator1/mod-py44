10ee40059cb37834226ec5b12700d12998a1c374
lagos-automator1 <somoriboy@gmail.com>
2024-01-22 01:20:53 +0000

    adds more features

--- check-file-creation-date.py
+++ check-file-creation-date.py
@@ -10,10 +10,15 @@ import time
 
 def main():
     print(os.name)
-
+    
+    # Gets user input - value of file to check
     user_input = str(input("Welcome to PhunkyTech's file checker\nType name of file to check: "))
+    # get the modification time of the file using path module
+    # then convert to readable time using the time class's ctime function
     t = time.ctime(path.getmtime(user_input))
     print("This file was created on ", t)
+    # constructs datetime object using get modification time function(getmtime)
+    print (datetime.datetime.fromtimestamp(path.getmtime(user_input)))
 
 
 if __name__ == "__main__":
